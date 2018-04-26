88d3ced9506b36fa240fe5b79da4a84d73073c0c
kasiprasath <kasiprasath@gmail.com>
2018-04-26 17:26:45 +0530

    New Empty Project
    It has boot,jpa,mysql and web dependencies.

---
+++ src/main/java/com/srd/billpayment/srdBillpayment/SrdBillpaymentApplication.java
@@ -0,0 +1,12 @@
+package com.srd.billpayment.srdBillpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrdBillpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrdBillpaymentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/srd/billpayment/srdBillpayment/SrdBillpaymentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.srd.billpayment.srdBillpayment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SrdBillpaymentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
